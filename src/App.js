5f9db22f4089884e5d3756563802f07309ad805d
prafulla-kr <prafullakumar011@gmail.com>
2021-06-20 17:42:07 +0530

    basic pagination added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React,{useState,useEffect} from "react";
 
 const App=()=> {
   const [customers,setCustomers] = useState([]);
+  const [customers_2,setCustomers_2] = useState([]);
+  const [numPosts,setnumPosts] = useState(13);
   const [toggle,setToggle] = useState(false);
 
   useEffect(()=>{
     fetch("https://intense-tor-76305.herokuapp.com/merchants")
     .then(response=>response.json())
-    .then(json=>setCustomers(json))
-  })
+    .then(json=>{setCustomers(json);
+    setCustomers_2(json);}
+    )
+  },[])
  
+  const handleClick=(e)=> {
+    setnumPosts(e.target.value);
+    setCustomers_2(customers.slice(0, e.target.value));
+  }
   const generateCustomers=(i)=>{
     let maxBid = -1;
     let minBid = 100000;
@@ -38,9 +46,25 @@ const App=()=> {
   return (
     <div className="App">
       <button onClick={()=>setToggle(!toggle)}>Toggle</button>
-        {customers.map((i) => {
+        {customers_2.map((i) => {
           return generateCustomers(i);
         })}
+        <select
+          style={{
+            float: "right",
+            marginBottom: "20px",
+            fontSize: "25px",
+            backgroundColor: "blue",
+            color: "white",
+            padding: "10px"
+          }}
+          value={numPosts}
+          onChange={handleClick}
+        >
+          <option value={5}>{5}</option>
+          <option value={10}>{10}</option>
+          <option value={13}>{13}</option>
+        </select>
     </div>
   );
 }
