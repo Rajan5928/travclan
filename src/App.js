43fb7dbc53f6209dd89e5eb322c7484b8e0d04d8
prafulla-kr <prafullakumar011@gmail.com>
2021-06-20 17:13:59 +0530

    project setup with toggle and sorting functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
 
-function App() {
+const App=()=> {
+  const [customers,setCustomers] = useState([]);
+  const [toggle,setToggle] = useState(false);
+
+  useEffect(()=>{
+    fetch("https://intense-tor-76305.herokuapp.com/merchants")
+    .then(response=>response.json())
+    .then(json=>setCustomers(json))
+  })
+ 
+  const generateCustomers=(i)=>{
+    let maxBid = -1;
+    let minBid = 100000;
+    if (i.bids.length === 0) {
+      maxBid = "null";
+      minBid = "null";
+    } else {
+      for (let j of i.bids) {
+        if (j.amount > maxBid) {
+          maxBid = j.amount;
+        }
+        if (j.amount < minBid) {
+          minBid = j.amount;
+        }
+      }
+    }
+    return (
+      <p>
+        <div key={i.id}>
+          {i.firstname} {i.lastname}
+        </div>
+        <div>{toggle ? minBid : maxBid}</div>
+      </p>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>setToggle(!toggle)}>Toggle</button>
+        {customers.map((i) => {
+          return generateCustomers(i);
+        })}
     </div>
   );
 }
